78debebe6ee205218204191c21f302ed44315915
Meno Abels <meno.abels@adviser.com>
2014-10-14 07:07:50 +0200

    improved the switch

--- construct/templates.rb
+++ construct/templates.rb
@@ -1,15 +1,22 @@
 
 module Construct
   module Templates
-    @templates = []
+    @templates = {}
     class Template < OpenStruct
       def initialize(cfg)
         super(cfg)
       end
     end
+    def self.find(name)
+      ret = @templates[name]
+      throw "template with name #{name} not found" unless @templates[name]
+      ret
+    end
     def self.add(name, cfg)
+      throw "template with name #{name} exists" if @templates[name]
+      cfg['name'] = name
       ret = Template.new(cfg)
-      @templates << ret
+      @templates[name] = ret
       ret
     end
   end
