c6b4c055fc65b36ce29505b318c80e70fac459da
Marcel Richter <marcel.richter@sinnerschrader.com>
2014-11-07 18:19:35 +0100

    apply config support added

--- switch-chatter/src/main/java/com/sinnerschrader/construct/switchchatter/ApplyConfig.java
+++ switch-chatter/src/main/java/com/sinnerschrader/construct/switchchatter/ApplyConfig.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-public class Main {
+public class ApplyConfig {
 	public static void main(String[] args) throws UnknownHostException,
 			IOException, InterruptedException {
 		Socket socket = new Socket("172.16.252.252", 23);
@@ -18,17 +18,27 @@ public class Main {
 		Future<List<String>> result = sc.createOutputConsumerAndFutureResult();
 		sc.skipSplashScreen();
 		sc.setupTerminal();
-		sc.retrieveConfig();
+		sc.applyConfig("vlan 5000000\nvlan 1000000\nexit\n");
 		sc.exit();
 
 		try {
 			List<String> results = result.get(3, TimeUnit.SECONDS);
-
-			String config = results.get(0);
-			System.out.println(config);
+			int errors = 0;
+			for (String line : results) {
+				int errorMessage = line.indexOf("Invalid");
+				if (errorMessage >= 0) {
+					System.err.println(line.substring(errorMessage,
+							line.indexOf("\n")));
+				}
+				errors++;
+			}
+			if (errors > 0) {
+				System.exit(1);
+			}
 		} catch (Exception e) {
 			System.err.println("fatal error occured:");
 			e.printStackTrace(System.err);
+			System.exit(2);
 		} finally {
 			sc.close();
 			socket.close();
