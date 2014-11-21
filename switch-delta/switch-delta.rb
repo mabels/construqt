5905f3bb18dc752820cea0cf1c1d75003ef1e8f3
Marcel Richter <marcel.richter@sinnerschrader.com>
2014-11-21 15:40:08 +0100

    use construct datamodel

--- switch-delta/switch-delta.rb
+++ switch-delta/switch-delta.rb
@@ -1,35 +1,47 @@
-require_relative("configmodel.rb")
-require_relative("flavour.rb")
-require_relative("flavour_#{ARGV[0]}.rb")
+CONSTRUCT_PATH=ENV['CONSTRUCT_PATH']||'.'
+["#{CONSTRUCT_PATH}/ipaddress/lib","#{CONSTRUCT_PATH}/construct"].each{|path| $LOAD_PATH.unshift(path) }
 
-#remove flavour argument
-flavour = ARGV.shift
-parser = Flavour.parsers[flavour].new
-renderer = Flavour.renderers[flavour].new
+require("construct/construct.rb")
+require("construct/flavour/ciscian/ciscian.rb")
 
-swap = ARGV[0] == "swap"
-if (swap)
-  ARGV.shift
+require("switch-delta/configmodel.rb")
+require("switch-delta/flavour.rb")
+require("switch-delta/flavour_#{ARGV[0]}.rb")
+
+module Construct
+  module SwitchDelta
+
+    #remove flavour argument
+    flavour = ARGV.shift
+    parser = Flavour.parsers[flavour].new
+    renderer = Flavour.renderers[flavour].new
+
+    swap = ARGV[0] == "swap"
+    if (swap)
+      ARGV.shift
+    end
+
+    oldConfig = []
+    while ( line = $stdin.gets )
+      oldConfig << line.chomp
+    end
+    oldSwitchConfig = parser.parse(oldConfig)
+
+    newConfig = []
+    while ( line = ARGF.gets )
+      newConfig << line
+    end
+    newSwitchConfig = parser.parse(newConfig)
+
+    if (swap)
+      temp=newSwitchConfig
+      newSwitchConfig=oldSwitchConfig
+      oldSwitchConfig=temp
+    end
+
+    delta = newSwitchConfig.compare(oldSwitchConfig)
+
+    puts renderer.buildConfig(oldSwitchConfig, newSwitchConfig, delta)
+
+  end
 end
-
-oldConfig = []
-while ( line = $stdin.gets )
-  oldConfig << line.chomp
-end
-oldSwitchConfig = parser.parse(oldConfig)
-
-newConfig = []
-while ( line = ARGF.gets )
-  newConfig << line
-end
-newSwitchConfig = parser.parse(newConfig)
-
-if (swap)
-  temp=newSwitchConfig
-  newSwitchConfig=oldSwitchConfig
-  oldSwitchConfig=temp
-end
-
-delta = newSwitchConfig.compare(oldSwitchConfig)
-
-puts renderer.buildConfig(oldSwitchConfig, newSwitchConfig, delta)
