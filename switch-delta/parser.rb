96fd712bb7780765d542e582d541ce44eb3bc172
Marcel Richter <marcel.richter@sinnerschrader.com>
2014-11-21 15:40:08 +0100

    use construct datamodel

--- switch-delta/parser.rb
+++ switch-delta/parser.rb
@@ -1,27 +1,32 @@
-class SwitchConfigParser
-  PORTS_DEF_REGEXP = "([^\\d\\s]*\\d+|,|-)+"
-  PORT_NAME_REGEXP="^(.*?)(\\d+)$"
-  def resolvePortDefinition(portDef)
-    ports = portDef.split(",").map do |rangeDef|
-      range = rangeDef.split("-")
-      if (range.length==1)
-        range
-      elsif (range.length==2)
-        range[0]=~/#{PORT_NAME_REGEXP}/
-        prefixFrom=$1
-        from = $2
-        range[1]=~/#{PORT_NAME_REGEXP}/
-        prefixTo=$1
-        to = $2
-        throw "port prefixes differ" unless prefixFrom==prefixTo
-        (from.to_i .. to.to_i).map {|n| prefixFrom + n.to_s }
-      else
-        throw "invalid range found #{rangeDef}"
+module Construct
+  module SwitchDelta
+
+    class SwitchConfigParser
+      PORTS_DEF_REGEXP = "([^\\d\\s]*\\d+|,|-)+"
+      PORT_NAME_REGEXP="^(.*?)(\\d+)$"
+      def resolvePortDefinition(portDef)
+        ports = portDef.split(",").map do |rangeDef|
+          range = rangeDef.split("-")
+          if (range.length==1)
+            range
+          elsif (range.length==2)
+            range[0]=~/#{PORT_NAME_REGEXP}/
+            prefixFrom=$1
+            from = $2
+            range[1]=~/#{PORT_NAME_REGEXP}/
+            prefixTo=$1
+            to = $2
+            throw "port prefixes differ" unless prefixFrom==prefixTo
+            (from.to_i .. to.to_i).map {|n| prefixFrom + n.to_s }
+          else
+            throw "invalid range found #{rangeDef}"
+          end
+        end
+        ports.flatten
+      end
+      def parse(lines)
+        throw "this method must be implemented in specific flavour"
       end
     end
-    ports.flatten
-  end
-  def parse(lines)
-    throw "this method must be implemented in specific flavour"
   end
 end
