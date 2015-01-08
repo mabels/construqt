95786b19f256618deee81909aa4e675f19eab5b7
Meno Abels <meno.abels@adviser.com>
2015-01-08 22:19:29 +0100

    allow null services

--- lib/construqt/flavour/ubuntu/flavour_ubuntu_services.rb
+++ lib/construqt/flavour/ubuntu/flavour_ubuntu_services.rb
@@ -180,12 +180,30 @@ RADV
           end
         end
 
+        class Null
+          def initialize(service)
+          end
+          def up(ifname)
+          end
+
+          def down(ifname)
+          end
+
+          def vrrp(host, ifname, iface)
+          end
+
+          def interfaces(host, ifname, iface, writer)
+          end
+        end
+
         def self.get_renderer(service)
           factory = {
             Construqt::Services::DhcpV4Relay => DhcpV4Relay,
             Construqt::Services::DhcpV6Relay => DhcpV6Relay,
             Construqt::Services::Radvd => Radvd,
             Construqt::Services::ConntrackD => ConntrackD,
+            Construqt::Services::IpsecStartStop => Null,
+            Construqt::Services::BgpStartStop => Null,
             Construqt::Flavour::Ubuntu::Vrrp::RouteService => RouteService
           }
           found = factory.keys.find{ |i| service.kind_of?(i) }
