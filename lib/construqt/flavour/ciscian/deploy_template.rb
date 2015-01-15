7d7f8f73431ea2f54ffd25edd1f049ad64fe74c3
Marcel Richter <marcel.richter@gmail.com>
2015-01-15 14:56:28 +0100

    deployscript hardened for windows cygwin users

--- lib/construqt/flavour/ciscian/deploy_template.rb
+++ lib/construqt/flavour/ciscian/deploy_template.rb
@@ -12,6 +12,12 @@ then
 CURRENT_DIR="$(dirname $0)"
 fi
 
+SWITCH_IP=#{ip}
+if [ ! -z "$1" ]
+then
+  SWITCH_IP=$1
+fi
+
 if [ -z "$CONSTRUQT_PATH" ]
 then
 if [[ $CURRENT_DIR =~ (.*construqt).* ]]
@@ -25,7 +31,7 @@ fi
 
 FLAVOUR="#{flavour}"
 cd $CONSTRUQT_PATH
-bash $CONSTRUQT_PATH/construqt/switch/deploy/deploy.sh $FLAVOUR #{ip} #{user} #{pass} "$CURRENT_DIR/$FLAVOUR.cfg"
+bash $CONSTRUQT_PATH/construqt/switch/deploy/deploy.sh $FLAVOUR $SWITCH_IP #{user} "$CURRENT_DIR/$FLAVOUR.cfg"
 cd - > /dev/null
 TEMPLATE
                 Util.write_str(template, File.join(host.name, "deploy.sh"))
